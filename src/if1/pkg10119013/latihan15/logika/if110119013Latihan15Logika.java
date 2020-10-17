f2ceec2ef00ddc11a56aad5fe868a03bfb7a3035
FionaAvila <fionaavila15@gmail.com>
2020-10-17 18:38:48 +0700

    Initial Commit

---
+++ src/if1/pkg10119013/latihan15/logika/if110119013Latihan15Logika.java
@@ -0,0 +1,30 @@
+/*
+ * NAMA  : Fiona Avila Putri
+ * NIM   : 10119013
+ * KELAS : IF1 2019/2020 (PBO1)
+ */
+
+
+
+package if1.pkg10119013.latihan15.logika;
+
+/**
+ *
+ * @author Fiona Avila
+ */
+
+
+public class if110119013Latihan15Logika {
+
+    public static void main(String[] args) {
+        
+        boolean a = true;
+        boolean b = false;
+        
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
